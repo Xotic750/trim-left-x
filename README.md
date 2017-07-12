77c4e3aaee52149b211f5d96044acb8e934d32d9
Xotic750 <xotic750@gmail.com>
2017-07-12 15:17:37 +0200

    Update dependencies

--- README.md
+++ README.md
@@ -1,47 +1,43 @@
-<a name="module_trim-left-x"></a>
-
-## trim-left-x
 <a href="https://travis-ci.org/Xotic750/trim-left-x"
-title="Travis status">
+   title="Travis status">
 <img
-src="https://travis-ci.org/Xotic750/trim-left-x.svg?branch=master"
-alt="Travis status" height="18">
+   src="https://travis-ci.org/Xotic750/trim-left-x.svg?branch=master"
+   alt="Travis status" height="18"/>
 </a>
 <a href="https://david-dm.org/Xotic750/trim-left-x"
-title="Dependency status">
+   title="Dependency status">
 <img src="https://david-dm.org/Xotic750/trim-left-x.svg"
-alt="Dependency status" height="18"/>
+   alt="Dependency status" height="18"/>
 </a>
-<a
-href="https://david-dm.org/Xotic750/trim-left-x#info=devDependencies"
-title="devDependency status">
+<a href="https://david-dm.org/Xotic750/trim-left-x#info=devDependencies"
+   title="devDependency status">
 <img src="https://david-dm.org/Xotic750/trim-left-x/dev-status.svg"
-alt="devDependency status" height="18"/>
+   alt="devDependency status" height="18"/>
 </a>
 <a href="https://badge.fury.io/js/trim-left-x" title="npm version">
 <img src="https://badge.fury.io/js/trim-left-x.svg"
-alt="npm version" height="18">
+   alt="npm version" height="18"/>
 </a>
+<a name="module_trim-left-x"></a>
 
+## trim-left-x
 This method removes whitespace from the left end of a string.
 
-Requires ES3 or above.
-
-**Version**: 1.2.0  
+**Version**: 1.3.0  
 **Author**: Xotic750 <Xotic750@gmail.com>  
 **License**: [MIT](&lt;https://opensource.org/licenses/MIT&gt;)  
 **Copyright**: Xotic750  
 <a name="exp_module_trim-left-x--module.exports"></a>
 
 ### `module.exports(string)` ⇒ <code>undefined</code> \| <code>string</code> ⏏
-This method removes whitespace from the left end of a string
+This method removes whitespace from the left end of a string.
 
 **Kind**: Exported function  
 **Returns**: <code>undefined</code> \| <code>string</code> - The left trimmed string.  
 
 | Param | Type | Description |
 | --- | --- | --- |
-| string | <code>String</code> | The string to trim the left end whitespace from. |
+| string | <code>string</code> | The string to trim the left end whitespace from. |
 
 **Example**  
 ```js
--- index.js
+++ index.js
@@ -1,61 +1,27 @@
 /**
- * @file
- * <a href="https://travis-ci.org/Xotic750/trim-left-x"
- * title="Travis status">
- * <img
- * src="https://travis-ci.org/Xotic750/trim-left-x.svg?branch=master"
- * alt="Travis status" height="18">
- * </a>
- * <a href="https://david-dm.org/Xotic750/trim-left-x"
- * title="Dependency status">
- * <img src="https://david-dm.org/Xotic750/trim-left-x.svg"
- * alt="Dependency status" height="18"/>
- * </a>
- * <a
- * href="https://david-dm.org/Xotic750/trim-left-x#info=devDependencies"
- * title="devDependency status">
- * <img src="https://david-dm.org/Xotic750/trim-left-x/dev-status.svg"
- * alt="devDependency status" height="18"/>
- * </a>
- * <a href="https://badge.fury.io/js/trim-left-x" title="npm version">
- * <img src="https://badge.fury.io/js/trim-left-x.svg"
- * alt="npm version" height="18">
- * </a>
- *
- * This method removes whitespace from the left end of a string.
- *
- * Requires ES3 or above.
- *
- * @version 1.2.0
+ * @file This method removes whitespace from the left end of a string.
+ * @version 1.3.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module trim-left-x
  */
 
-/* eslint strict: 1 */
+'use strict';
 
-/* global module */
+var $toString = require('to-string-x');
+var reLeft = new RegExp('^[' + require('white-space-x').string + ']+');
 
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var $toString = require('to-string-x');
-  var ws = require('white-space-x').ws;
-  var reLeft = new RegExp('^[' + ws + ']+');
-
-  /**
-   * This method removes whitespace from the left end of a string
-   *
-   * @param {String} string The string to trim the left end whitespace from.
-   * @return {undefined|string} The left trimmed string.
-   * @example
-   * var trimLeft = require('trim-left-x');
-   *
-   * trimLeft(' \t\na \t\n') === 'a \t\n'; // true
-   */
-  module.exports = function trimLeft(string) {
-    return $toString(string).replace(reLeft, '');
-  };
-}());
+/**
+ * This method removes whitespace from the left end of a string.
+ *
+ * @param {string} string - The string to trim the left end whitespace from.
+ * @returns {undefined|string} The left trimmed string.
+ * @example
+ * var trimLeft = require('trim-left-x');
+ *
+ * trimLeft(' \t\na \t\n') === 'a \t\n'; // true
+ */
+module.exports = function trimLeft(string) {
+  return $toString(string).replace(reLeft, '');
+};
