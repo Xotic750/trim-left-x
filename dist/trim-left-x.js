77ff4aaf89aecd19422f3d65b7878b4017915857
Graham Fairweather <xotic750@gmail.com>
2019-07-27 02:25:37 +0200

    :bookmark: v4.0.26
    Auto build

--- dist/trim-left-x.js
+++ dist/trim-left-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-26T12:45:30.182Z",
+  "date": "2019-07-27T00:25:32.673Z",
   "describe": "",
   "description": "This method removes whitespace from the left end of a string.",
   "file": "trim-left-x.js",
   "hash": "2118989b4cdcea226cec",
   "license": "MIT",
-  "version": "4.0.25"
+  "version": "4.0.26"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
